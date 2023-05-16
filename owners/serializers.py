c5ba7a7c22e60919ed71c0cdc8adfb9da52d9130
Robyn Smith <robyn.roybin@gmail.com>
2023-05-16 20:05:52 +0000

    Added Owner permission and login/out views

--- owners/serializers.py
+++ owners/serializers.py
@@ -4,10 +4,15 @@ from .models import Owner
 
 class OwnerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Owner
         fields = [
             'id', 'owner', 'name', 'created_at', 'updated_at',
-            'image', 'about',
+            'image', 'about', 'is_owner',
         ]
--- owners/views.py
+++ owners/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Owner
 from .serializers import OwnerSerializer
+from bp_api.permissions import IsOwnerOrReadOnly
 
 
 class OwnerList(APIView):
@@ -15,21 +16,28 @@ class OwnerList(APIView):
 
 class OwnerDetail(APIView):
     serializer_class = OwnerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             owner = Owner.objects.get(pk=pk)
+            self.check_object_permissions(self.request, owner)
             return owner
         except Owner.DoesNotExist:
             raise Http404
     
     def get(self, request, pk):
         owner = self.get_object(pk)
-        serializer = OwnerSerializer(owner)
+        serializer = OwnerSerializer(
+            owner, context={'request': request}
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
         owner = self.get_object(pk)
-        serializer = OwnerSerializer(owner, data=request.data)
+        serializer = OwnerSerializer(
+            owner, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)

d7a0572951ead6792b6e7f85c28f3960047e854f
Robyn Smith <robyn.roybin@gmail.com>
2023-05-17 19:02:15 +0000

    Added pet detail view and update/delete methods

--- pets/urls.py
+++ pets/urls.py
@@ -3,4 +3,5 @@ from pets import views
 
 urlpatterns = [
     path('pets/', views.PetList.as_view()),
+    path('pets/<int:pk>/', views.PetDetail.as_view()),
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Pet
 from .serializers import PetSerializer
+from bp_api.permissions import IsOwnerOrReadOnly
 
 
 class PetList(APIView):
@@ -32,3 +34,38 @@ class PetList(APIView):
         )
 
 
+class PetDetail(APIView):
+    serializer_class = PetSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            pet = Pet.objects.get(pk=pk)
+            self.check_object_permissions(self.request, pet)
+            return pet
+        except Pet.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        pet = self.get_object(pk)
+        serializer = PetSerializer(
+            pet, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        pet = self.get_object(pk)
+        serializer = PetSerializer(
+            pet, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        pet = self.get_object(pk)
+        pet.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )

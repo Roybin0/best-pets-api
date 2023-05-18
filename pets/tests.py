1e2a6649eaf298f0d79514473758ab0619c69cda
Robyn Smith <robyn.roybin@gmail.com>
2023-05-18 18:26:17 +0000

    Added more testing for pets app

--- pets/tests.py
+++ pets/tests.py
@@ -14,7 +14,7 @@ class PetListViewTest(APITestCase):
         response = self.client.get('/pets/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_pet(self):
+    def test_create_pet_authenticated(self):
         data = {
             'name': 'Luna',
             'pet_type': 'Cat',
@@ -27,53 +27,70 @@ class PetListViewTest(APITestCase):
         self.assertEqual(response.data['pet_type'], 'Cat')
         self.assertEqual(count, 1)
 
-    # def test_create_pet_unauthenticated(self):
-    #     url = '/pets/'
-    #     data = {
-    #         'name': 'Fluffy',
-    #         'pet_type': 'Cat',
-    #         'image': '<image-data>',
-    #         'about': 'A cute cat'
-    #     }
-    #     self.client.force_authenticate(user=None)
-    #     response = self.client.post(url, data, format='multipart')
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_create_pet_unauthenticated(self):
+        data = {
+            'name': 'Fluffy',
+            'pet_type': 'Cat',
+            'image': '<image-data>',
+            'about': 'A cute cat'
+        }
+        response = self.client.post('/pets/', data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    # def test_create_pet_invalid_data(self):
-    #     url = '/pets/'
-    #     data = {
-    #         'name': '',  # Invalid name (empty)
-    #         'pet_type': 'Cat',
-    #         'image': '<image-data>',
-    #         'about': 'A cute cat'
-    #     }
-    #     response = self.client.post(url, data, format='multipart')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIn('name', response.data)
+    def test_create_pet_invalid_data(self):
+        data = {
+            'name': '',  # Invalid name (empty)
+            'pet_type': 'Cat',
+            'image': '<image-data>',
+            'about': 'A cute cat'
+        }
+        self.client.login(username='test', password='pass')
+        response = self.client.post('/pets/', data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('name', response.data)
 
-    # def test_retrieve_pets(self):
-    #     url = '/pets/'
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     # Add more assertions to verify the response data
+    def test_list_all_pets(self):
+        response = self.client.get('/pets/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Add more assertions to verify the response data
 
-    # def test_retrieve_specific_pet(self):
-    #     pet = Pet.objects.create(owner=self.user, name='Fluffy', pet_type='Cat')
-    #     url = f'/pets/{pet.pk}/'
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data['name'], 'Fluffy')
+    def test_get_pet_by_id(self):
+        user = User.objects.get(username='test')
+        pet = Pet.objects.create(owner=user, name='Luna', pet_type='Cat')
+        url = f'/pets/{pet.pk}/' 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Luna')
 
-    # def test_retrieve_pet_unauthenticated(self):
-    #     pet = Pet.objects.create(owner=self.user, name='Fluffy', pet_type='Cat')
-    #     url = f'/pets/{pet.pk}/'
-    #     self.client.force_authenticate(user=None)
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_cant_edit_pet_unauthenticated(self):
+        user = User.objects.get(username='test')
+        pet = Pet.objects.create(owner=user, name='Luna', pet_type='Cat')
+        url = f'/pets/{pet.pk}/'
+        self.client.force_authenticate(user=None)
+        response = self.client.put(url, name='Fluffy')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    # def test_retrieve_pet_by_owner(self):
-    #     pet = Pet.objects.create(owner=self.user, name='Fluffy', pet_type='Cat')
-    #     url = f'/pets/{pet.pk}/'
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data['owner'], self.user.username)
+    def test_can_edit_pet_authenticated(self):
+        user = User.objects.get(username='test')
+        pet = Pet.objects.create(owner=user, name='Luna', pet_type='Cat')
+        url = f'/pets/{pet.pk}/'
+        self.client.login(username='test', password='pass')
+        response = self.client.put(url, data={'name': 'Fluffy', 'pet_type': 'Cat'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['owner'], user.username)
+    
+    def test_cant_delete_pet_unauthenticated(self):
+        user = User.objects.get(username='test')
+        pet = Pet.objects.create(owner=user, name='Luna', pet_type='Cat')
+        url = f'/pets/{pet.pk}/'
+        self.client.force_authenticate(user=None)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_can_delete_pet_authenticated(self):
+        user = User.objects.get(username='test')
+        pet = Pet.objects.create(owner=user, name='Luna', pet_type='Cat')
+        url = f'/pets/{pet.pk}/'
+        self.client.login(username='test', password='pass')
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)

7785f96bccc2f5e9658a63483d02063cbbc4b7f3
Robyn Smith <robyn.roybin@gmail.com>
2023-05-20 13:10:58 +0000

    Fixed comment serializer and removed pet from comments model

--- comments/models.py
+++ comments/models.py
@@ -6,12 +6,11 @@ from petpics.models import PetPic
 
 class Comment(models.Model):
     """
-    Comment model related to User. May also be associated with
-    pets, pet tales or pet stories.  
+    Comment model related to User. May also be associated 
+    with pet tales or pet stories.  
     """
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, blank=True, null=True)
     pet_tale = models.OneToOneField(PetTale, on_delete=models.CASCADE, blank=True, null=True)
     pet_pic = models.OneToOneField(PetPic, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- comments/serializers.py
+++ comments/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from .models import Comment
-from pets.models import Pet
 from pettales.models import PetTale
 from petpics.models import PetPic
-from pets.serializers import PetSerializer
 from pettales.serializers import PetTaleSerializer
 from petpics.serializers import PetPicSerializer
 
@@ -13,54 +11,36 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     owner_id = serializers.ReadOnlyField(source='owner.owner.id')
     owner_profile_image = serializers.ReadOnlyField(source='owner.owner.image.url')
-    pet = serializers.PrimaryKeyRelatedField(read_only=True)
-    pet_id = serializers.PrimaryKeyRelatedField(queryset=Pet.objects.all(), write_only=True, required=False)
-    pet_tale_id = serializers.PrimaryKeyRelatedField(queryset=PetTale.objects.all(), write_only=True, required=False)
-    pet_pic_id = serializers.PrimaryKeyRelatedField(queryset=PetPic.objects.all(), write_only=True, required=False)
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-
-    def  validate(self, attrs):
-        pet_id = attrs.get('pet_id')
-        pet_tale_id = attrs.get('pet_tale')
-        pet_pic_id = attrs.get('pet_pic')
-        
-        if pet_id:
-            if pet_tale_id and pet_tale_id.pet != pet_id:
-                raise serializers.ValidationError("The selected pet tale does not belong to the chosen pet.")
-            if pet_pic_id and pet_pic_id.pet != pet_id:
-                raise serializers.ValidationError("The selected pet pic does not belong to the chosen pet.")
-
-        return attrs
     
     def create(self, validated_data):
-        pet_id = validated_data.pop('pet_id', None)
-        
-        comment = Comment.objects.create(**validated_data)
+        request = self.context['request']
+        validated_data['owner'] = request.user
 
-        if pet_id:
-            try:
-                pet = Pet.objects.get(pk=pet_id)
-                comment.pet = pet
-            except Pet.DoesNotExist:
-                raise serializers.ValidationError("Invalid Pet ID")
+        # Check if comment is for a PetTale or PetPic
+        if 'pet_tale' in validated_data:
+            pet_tale = validated_data.pop('pet_tale')
+            if pet_tale:
+                validated_data['pet_tale_id'] = pet_tale.id
+        elif 'pet_pic' in validated_data:
+            pet_pic = validated_data.pop('pet_pic')
+            if pet_pic:
+                validated_data['pet_pic_id'] = pet_pic.id
 
-        comment.save()
-
-        return comment
+        return Comment.objects.create(**validated_data)
     
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'pet', 'pet_id', 'pet_tale_id', 'pet_pic_id',
+            'id', 'owner', 'pet_tale', 'pet_pic',
             'created_at', 'updated_at', 'comment', 'is_owner', 'owner_id',
             'owner_profile_image',
         ]
 
 
 class CommentDetailSerializer(CommentSerializer):
-    pet = PetSerializer(read_only=True)
     pet_tale = PetTaleSerializer(read_only=True)
     pet_pic = PetPicSerializer(read_only=True)

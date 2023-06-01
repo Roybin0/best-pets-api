2c58df120ffb9379c5f90dd7f78b75994973c0c6
Robyn Smith <robyn.roybin@gmail.com>
2023-06-01 21:48:56 +0000

    Fixed comment model ForeignKeys

--- comments/models.py
+++ comments/models.py
@@ -7,12 +7,12 @@ from petpics.models import PetPic
 class Comment(models.Model):
     """
     Comment model related to User. May also be associated 
-    with pet tales or pet stories.  
+    with pet tales or pet pics.  
     """
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    pet_tale = models.OneToOneField(PetTale, on_delete=models.CASCADE, blank=True, null=True)
-    pet_pic = models.OneToOneField(PetPic, on_delete=models.CASCADE, blank=True, null=True)
+    pet_tale = models.ForeignKey(PetTale, on_delete=models.CASCADE, blank=True, null=True)
+    pet_pic = models.ForeignKey(PetPic, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     comment = models.TextField(blank=False)

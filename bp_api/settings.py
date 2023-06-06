48976c140760f87799f55890623c3913834fa173
Robyn Smith <robyn.roybin@gmail.com>
2023-06-06 20:14:40 +0000

    Update CORS for Code Anywhere migration

--- bp_api/settings.py
+++ bp_api/settings.py
@@ -122,9 +122,10 @@ if 'CLIENT_ORIGIN' in os.environ:
         'http://localhost:3000',
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    extracted_url = re.match(r'^([^.]+)', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
